19e11689d626b5429903e4bdb245a498b8ec3d68
8bithermitcrab <kij517@naver.com>
2022-02-26 22:45:16 +0900

    220226

--- 5_1차원 배열/BOJ_2577.py
+++ 5_1차원 배열/BOJ_2577.py
@@ -1,1 +1,8 @@
 # 숫자의 개수
+
+a, b, c = [int(input()) for _ in range(3)]
+
+num = list(str(a * b * c))
+
+for i in range(10):
+    print(num.count(str(i)))
--- BOJ_2447.py
+++ BOJ_2447.py
@@ -1,14 +1,40 @@
 # 별 찍기 - 10
 
-star = '***\n* *\n***'
+n = int(input())
+board = [[0 for i in range(n)] for i in range(n)]
+
+# 별 찍는 함수
+def draw_star(n):
+    if n == 3:
+        # 1행 = 3행 = [1, 1, 1]
+        board[0][:3] = board[2][:3] = [1] * 3
+        # 2행 = [1, 0, 1]
+        board[1][:3] = [1, 0, 1]
+        return
+
+    a = n//3
+    draw_star(a)
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                continue
+            for k in range(a):
+                board[a] 
+
+
+
+
+'''
+star = ['***', '* *', '***']
 
 # n = 3^k
 # k = 3^n
-
-n = int(input())
-print(star*9, end='')
 
 # if n // 3 == 0:
 #     # (n/3)*(n/3) 크기의 정사각형
 #     # n/3의 패턴
 #     print(star*(n/3))
+
+
+https://study-all-night.tistory.com/5
+'''
