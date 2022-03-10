eb1eb25526e09778c6b976c0b694466e6fae5f5f
8bithermitcrab <kij517@naver.com>
2022-03-10 23:45:50 +0900

    220310

--- 이것이 코딩 테스트다/3-3.py
+++ 이것이 코딩 테스트다/3-3.py
@@ -1,3 +1,12 @@
 # p.98
 
-n, m
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+
+print(result)
