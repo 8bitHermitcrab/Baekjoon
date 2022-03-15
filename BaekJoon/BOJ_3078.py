03c6c20f8a02c5832f4f06b99d41b6e752996c6f
8bithermitcrab <kij517@naver.com>
2022-03-15 23:51:21 +0900

    220315

--- BaekJoon/BOJ_3078.py
+++ BaekJoon/BOJ_3078.py
@@ -1,18 +1,23 @@
 # 좋은 친구
 
+from sys import stdin
+
 # n줄의 학생, k등수 차이
-n, k = map(int, input().split())
+n, k = map(int, stdin.readline().split())
 students = [0] * n
 data = [0] * 21
 count = 0
 
 for rank in range(n):
     # 이름의 길이
-    name = len(input().strip())
-    # rank 인덱스에 name 값
+    name = len(stdin.readline().rstrip())
     students[rank] = name
     if rank > k:
         data[students[rank - k - 1]] -= 1
-        count += data[1]
-        data[1] += 1
+    count += data[name]
+    data[name] += 1
 print(count)
+
+'''
+https://derekahndev.github.io/problem%20solving/boj-3078/
+'''
