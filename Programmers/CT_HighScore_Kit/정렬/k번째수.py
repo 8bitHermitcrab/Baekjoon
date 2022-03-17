6192e2017ad09acc56385e92e52fdbc33b11c4dc
8bithermitcrab <kij517@naver.com>
2022-03-17 21:53:50 +0900

    220317

--- Programmers/CT_HighScore_Kit/정렬/k번째수.py
+++ Programmers/CT_HighScore_Kit/정렬/k번째수.py
@@ -1,4 +1,17 @@
-''' 오답정리
+''' 오답정리 '''
+
+def solution(array, commands):
+
+    answer = []
+
+    for command in commands:
+        new_array = array[command[0]-1:command[1]] # 문제에서 주어진 크기만큼 자르기
+        new_array.sort() # sort 함수로 정렬
+        answer.append(new_array[command[2]-1]) # k 번째 값 집어넣기
+
+    return answer
+
+'''
 [1, 5, 2, 6, 3, 7, 4]를 2번째부터 5번째까지 자른 후 정렬합니다. 
 [2, 3, 5, 6]의 세 번째 숫자는 5입니다.
 [1, 5, 2, 6, 3, 7, 4]를 4번째부터 4번째까지 자른 후 정렬합니다. 
@@ -20,11 +33,3 @@ print(answer)
 
 https://sinsomi.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-K%EB%B2%88%EC%A7%B8%EC%88%98
 '''
-
-
-
-
-for command in commands:
-    new_array = array[command[0]-1:command[1]]
-    new_array.sort()
-    answer.append(new_array[command[2]-1])
