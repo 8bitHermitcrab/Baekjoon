c67a346456794079501829dbd874dbc555e8ddb6
8bithermitcrab <kij517@naver.com>
2022-03-23 10:24:51 +0900

    220322

--- BaekJoon/9_기본 수학2/BOJ_2581.py
+++ BaekJoon/9_기본 수학2/BOJ_2581.py
@@ -6,18 +6,23 @@ n = int(input())
 # m~n 소수의 합
 # 소수 체크
 mn_list = list()
-for k in range(m, n+1):
-    if k == 1:
-        pass
-    elif k == 2:
-        mn_list.append(k)
-    for i in range(2, k):
-        if k % i == 0:
-            break
-        else i == k - 1:
-            mn_list.append(i)
 
-if len()
+for num in range(m, n+1):
+    error = 0
+    if num > 1:
+        for i in range(2, num): # 소수 탐색
+            if num % i == 0:
+                error += 1
+                break
+            # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
+        if error == 0:
+            mn_list.append(num)
 
-print(sum(mn_list))
-print(min(mn_list))
+if len(mn_list) > 0:
+    print(sum(mn_list))
+    print(min(mn_list))
+else:
+    print(-1)
+
+
+# https://ooyoung.tistory.com/93
