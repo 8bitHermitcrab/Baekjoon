4f537e0cc7e28cca29ff21c657152f20d55227a3
8bithermitcrab <kij517@naver.com>
2022-03-27 10:50:03 +0900

    220327

--- BaekJoon/9_기본 수학2/BOJ_1085.py
+++ BaekJoon/9_기본 수학2/BOJ_1085.py
@@ -3,3 +3,8 @@
 x, y, w, h = map(int, input().split())
 
 print(min([x, y, w-x, h-y]))
+
+
+
+
+# https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-1085%EB%B2%88-%EC%A7%81%EC%82%AC%EA%B0%81%ED%98%95%EC%97%90%EC%84%9C-%ED%83%88%EC%B6%9C-%ED%8C%8C%EC%9D%B4%EC%8D%ACPython
