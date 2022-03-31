74339fd48a342743e049d73347d22677b0d3d6bf
8bithermitcrab <kij517@naver.com>
2022-03-31 21:56:59 +0900

    220331

---
+++ BaekJoon/9_기본 수학2/BOJ_3053.py
@@ -0,0 +1,11 @@
+# 택시 기하학
+
+import math
+
+r = int(input())
+
+print(r*r *math.pi)
+print(f'{r*r *2 :.6f}')
+
+
+# https://leedakyeong.tistory.com/entry/%EB%B0%B1%EC%A4%80-3053%EB%B2%88-%ED%83%9D%EC%8B%9C-%EA%B8%B0%ED%95%98%ED%95%99-in-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%89%BD%EA%B2%8C-%ED%92%80%EC%96%B4%EB%B3%B4%EA%B8%B0
