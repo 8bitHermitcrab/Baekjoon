64863afbac6ebd8627326c5a8ddedfb8d6c036ce
8bithermitcrab <kij517@naver.com>
2022-04-05 23:26:50 +0900

    220405

--- BaekJoon/11_브루트 포스/BOJ_2798.py
+++ BaekJoon/11_브루트 포스/BOJ_2798.py
@@ -2,3 +2,19 @@
 
 n, m = map(int, input().split())
 
+num = list(map(int, input().split()))
+len_num = len(num)
+result = 0
+
+for i in range(len_num -2):
+    for j in range(i+1, len_num -1):
+        for k in range(j+1, len_num):
+            if(num[i] + num[j] + num[k] > m):
+                continue
+            else:
+                result = max(result, num[i] + num[j] + num[k])
+
+print(result)
+
+
+# https://velog.io/@jeongdopark/Algorithm-python-%EB%B0%B1%EC%A4%80-2798%EB%B2%88
