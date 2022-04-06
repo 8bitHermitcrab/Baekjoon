f1628901a8b2aaf9d821ef14ec0ffaf78d342e1a
8bithermitcrab <kij517@naver.com>
2022-04-06 19:28:34 +0900

    220406

---
+++ BaekJoon/11_브루트 포스/BOJ_7568.py
@@ -0,0 +1,18 @@
+# 덩치
+
+n = int(input())
+
+kg_cm = []
+
+for _ in range(n):
+    kg, cm = map(int, input().split())
+    kg_cm.append((kg, cm))
+
+# print(kg_cm)
+
+for i in kg_cm:
+    rank = 1
+    for j in kg_cm:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
