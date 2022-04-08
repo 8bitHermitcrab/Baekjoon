515600f01ab22419c957e9629f3a85f29bb86e61
8bithermitcrab <kij517@naver.com>
2022-04-09 01:57:42 +0900

    220409

---
+++ BaekJoon/12_정렬/BOJ_2751.py
@@ -0,0 +1,33 @@
+# 수 정렬하기 2
+
+
+n = int(input())
+num_list = []
+
+for _ in range(n):
+    num_list(int(input())
+
+num_list.sort()
+
+for data in num_list:
+    print(data)
+
+# https://leunco.tistory.com/m/71
+
+
+'''
+import sys
+
+n = int(sys.stdin.readline())
+
+num_list = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    num_list.append(num)
+
+# print(num_list)
+
+for i in range(n-1, -1, -1):
+    print(num_list[i])
+'''
