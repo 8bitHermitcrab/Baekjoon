22e48119d18530a10cdfa0e090593e16503bf9aa
8bitHermitcrab <8bithermitcrab@gmail.com>
2024-02-02 00:15:54 +0900

    Time: 32 ms (84.60%), Space: 16.3 MB (92.73%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -2,7 +2,7 @@ class Solution:
     def sortSentence(self, s: str) -> str:
         ans = ''
         s_list = s.split()
-        s_list.sort(key=lambda x : x[-1] )
+        s_list.sort(key=lambda x : x[-1])
         
         for i in s_list:
             ans += i[:-1] + ' '
