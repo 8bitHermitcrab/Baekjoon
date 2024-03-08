29a4dab76c272efbe6fb1a407bc5cfb4cb77d015
8bitHermitcrab <8bithermitcrab@gmail.com>
2024-03-08 21:20:33 +0900

    [Silver I] Title: 스타트링크, Time: 592 ms, Memory: 79692 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+def bfs(v):
+    q = deque([v])
+    visited[v] = 1
+    while q:
+        v = q.popleft()
+        if v == g:
+            return cnt[g]
+        for i in (v + u, v - d):
+            if 0 < i <= f and not visited[i]:
+                visited[i] = 1
+                cnt[i] = cnt[v] + 1
+                q.append(i)
+    if cnt[g] == 0:
+        return "use the stairs"
+
+input = sys.stdin.readline
+f, s, g, u, d = map(int, input().split())
+visited = [0 for i in range(f + 1)]
+cnt = [0 for i in range(f + 1)]
+print(bfs(s))
