3948a44c372cf3adc41389f918558ec2bc190f12
8bitHermitcrab <8bithermitcrab@gmail.com>
2024-03-09 14:21:22 +0900

    Time: 80 ms (92.44%), Space: 16.5 MB (95.15%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.py
@@ -0,0 +1,10 @@
+import heapq
+
+class Solution:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        for row in grid:
+            row.sort()
+        
+        trans_grid = zip(*grid)
+        sum_max_values = sum(max(column) for column in trans_grid)
+        return sum_max_values
