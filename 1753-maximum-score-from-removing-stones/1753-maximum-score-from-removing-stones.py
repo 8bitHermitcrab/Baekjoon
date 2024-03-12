b15bfb89e7f11dd081a03beeebb9eaa8c7db63ff
8bitHermitcrab <8bithermitcrab@gmail.com>
2024-03-12 16:51:43 +0900

    Time: 295 ms (28.57%), Space: 16.6 MB (59.14%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.py
@@ -0,0 +1,17 @@
+from heapq import heappush, heappop,heapify
+
+class Solution:
+    def maximumScore(self, a: int, b: int, c: int) -> int:
+        res = 0
+        stones = [-a, -b, -c]
+        heapify(stones)
+        
+        while abs(stones[0]) > 0 and abs(stones[1]) > 0:
+            first = heappop(stones) + 1
+            second = heappop(stones) + 1
+            
+            heappush(stones, first)
+            heappush(stones, second)
+            
+            res += 1
+        return res
