3b51145b7c267bd504e398fd1a1f9813875037cf
8bitHermitcrab <8bithermitcrab@gmail.com>
2024-04-23 18:46:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,21 @@
+def solution(number, k):
+    answer = []
+    
+    for num in number:
+        if not answer:
+            answer.append(num)
+            continue
+            
+        if k > 0:
+            while answer[-1] < num:
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0:
+                    break
+        answer.append(num)
+
+    if k > 0:
+        answer = answer[:-k]
+    else:
+        answer = answer
+    return ''.join(answer)
